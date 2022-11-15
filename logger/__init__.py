589ead77441190034213a8bb5714e944c12957db
abedaton <antoine.bedaton.ext@eurocontrol.int>
2022-11-15 16:27:22 +0100

    Moved to __init__ and colors to separate file

--- logger/__init__.py
+++ logger/__init__.py
@@ -3,17 +3,7 @@ import logging
 import logging.handlers
 import sys
 from functools import wraps
-
-
-class ColorCodes:
-    blue = "\u001b[38;5;152m"
-    green = "\u001b[38;5;10m"
-    orange = "\x1b[38;5;214m"
-    red = "\x1b[31;10m"
-    bold_red = "\x1b[31;1m"
-    light_blue = "\u001b[38;5;129m"
-    pink = "\u001b[38;5;219m"
-    reset = "\x1b[0m"
+from colors import ColorCodes
 
 
 class ConsoleFormatter(logging.Formatter):
---
+++ logger/colors.py
@@ -0,0 +1,9 @@
+class ColorCodes:
+    blue = "\u001b[38;5;152m"
+    green = "\u001b[38;5;10m"
+    orange = "\x1b[38;5;214m"
+    red = "\x1b[31;10m"
+    bold_red = "\x1b[31;1m"
+    light_blue = "\u001b[38;5;129m"
+    pink = "\u001b[38;5;219m"
+    reset = "\x1b[0m"
